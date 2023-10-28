4f475380037bcbdd55e5fac3f4b7a05a33b194b1
bibekjodd <bibekjodd@outlook.com>
2023-10-28 11:48:26 +0545

    chore: add metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import TransformedCode from "@/components/transformed-code/index";
 export default function page() {
   return (
     <main className=" flex min-h-screen flex-col overflow-hidden overflow-y-auto bg-[#282a36] px-3.5 pt-2 text-white scrollbar-hide sm:px-4 md:px-5 lg:px-20">
-      <div className="mx-auto h-full w-full flex-1 space-y-10 pt-2 md:grid md:grid-cols-2 md:space-x-10 md:space-y-0">
+      <div className="mx-auto h-full w-full flex-1 space-y-10 pt-2 md:grid md:grid-cols-2 md:space-y-0 lg:space-x-10">
         <CodeEditor />
         <TransformedCode />
       </div>
